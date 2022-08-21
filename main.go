556cdbce1d9ed111100aafba2aabc8f57291e487
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-08-22 02:32:37 +0530

    go web server

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello Go!!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		s := html.EscapeString(r.URL.Path)
+		fmt.Fprintf(w, "Hello, you are %q", s)
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	fmt.Println("Server is Running")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
