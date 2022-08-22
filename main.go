94e53f3771e711f841420a80e54836a3ae9c0eb0
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-08-22 11:11:42 +0530

    modified web server

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		s := html.EscapeString(r.URL.Path)
+		s = s[1:]
 		fmt.Fprintf(w, "Hello, you are %q", s)
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+		fmt.Fprintf(w, "Hi with GO!!")
 	})
 
 	fmt.Println("Server is Running")
